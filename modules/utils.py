fee6f21a527dc21c82ff9ded5a82f6e6742ec36a
Kelvin Do <kelvin.h.do@gmail.com>
2016-05-27 14:25:32 -0700

    Added kelvin token stuff and removed pyc

--- modules/utils.py
+++ modules/utils.py
@@ -7,6 +7,8 @@ from database import users
 
 PACKAGE_LENGTH = 300
 token = 'EAAYWALshr2kBAIVdPVwKscuFZAGjdB2dJzFx1qd8JA3bMnDTaZCr0fH0avAlPfLv72yBFEVtSXgXICMGWXZCbHS97OgZA5Q4qF0xdZAFRs0CtQ5HpMGUuNZCHJoewtR5ZCdKZA0UdHGwR6FZAETigcCYOU1bPH6xH00yfgV7ZASpGhbgZDZD'
+kelvin_token = 'EAAak1FpgZAygBAFkr1NOD2DJUwL4r3j74VScIDCMmW4bpBvfnoQi1VmS1KvZCFM0yooJ7Sisg8IUcioQReVAFt25RwberE8olwX8Vsre412IaNdc07fV4GDYIS4bsil4dJmRtp2r6nud8I8SeOGB8R6IO1I8E1td8QljAQNwZDZD'
+
 
 def send_response(message, user_id):
 	""" Constructs the request and sends a message to the user corresponding to the specified user id """
@@ -26,7 +28,7 @@ def send_response(message, user_id):
 			data = json.dumps({"recipient": {"id": user_id}, "message": {"text": to_send}})
 
 			#=====[ URL is to a general facebook messenger bot endpoint + access_token ]=====
-			url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token 
+			url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + kelvin_token 
 
 			#=====[ Construct headers ]=====
 			headers = {"Content-Type": "application/json"}
